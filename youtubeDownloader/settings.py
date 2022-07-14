af7f82e73fd564a9fb98625ee807e2d5ba8617cd
HelixyOfficial <hellixy03@gmail.com>
2022-07-15 00:34:40 +0530

    three

--- youtubeDownloader/settings.py
+++ youtubeDownloader/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-1hz4$r+$llqf1q_ak_*w8jfnglkc=alcfca7em^=pmak^a0h5(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
